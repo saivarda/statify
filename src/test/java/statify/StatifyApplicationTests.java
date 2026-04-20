87374d6833788f04c4bae42a5ebd8799623af96e
Sai Varda <126225887+saivarda@users.noreply.github.com>
2026-04-20 17:36:55 -0500

    feat: add spring boot project scaffold

---
+++ src/test/java/statify/StatifyApplicationTests.java
@@ -0,0 +1,13 @@
+package statify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
